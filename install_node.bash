2411133a548cbb6d4f2f78ab1dfa4187bc69d706
ritzvincentculanag <ritzvincentvergaraculanag@gmail.com>
2025-01-04 03:48:27 +0800

    add install script for node

---
+++ install_node.bash
@@ -0,0 +1,15 @@
+!/bin/bash
+
+chmod +x install_node.bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+
+nvm install 22
+
+node -v
+nvm current 
+
+npm -v
